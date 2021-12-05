e4b19ad599b2d8a01c7b8554db5d3218a8deeebe
lokn309 <lokn329@aliyun.com>
2021-12-05 17:04:04 +0800

    新增方法调用

--- week5/homework-spring-boot-starter/src/main/java/lokn/springboot/starter/StarterAutoConfiguration.java
+++ week5/homework-spring-boot-starter/src/main/java/lokn/springboot/starter/StarterAutoConfiguration.java
@@ -1,5 +1,9 @@
 package lokn.springboot.starter;
 
+import lokn.springboot.starter.bean.Klass;
+import lokn.springboot.starter.bean.School;
+import lokn.springboot.starter.bean.Student;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -10,6 +14,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class StarterAutoConfiguration {
 
+    @Bean
+    public Student student()  {
+        return new Student();
+    }
 
+    @Bean
+    public School school() {
+        return new School();
+    }
+
+    @Bean
+    public Klass klass() {
+        return new Klass();
+    }
 
 }
